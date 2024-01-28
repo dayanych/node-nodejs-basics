52b07cd3ad96eddae8dca0eef461af5b95f21ab4
dayanych <dayana.shakenova20@gmail.com>
2024-01-28 19:31:28 +0600

    feat: update cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+  const arr = [];
+
+  for (const key in envVariables) {
+    if (key.startsWith('RSS_')) {
+      arr.push(`${key}=${envVariables[key]}`);
+    }
+  }
+
+  console.log(arr.join('; '));
 };
 
 parseEnv();
