4b97a325ee13731ced1e5929a179a250e768e301
dayanych <dayana.shakenova20@gmail.com>
2024-01-28 21:11:50 +0600

    feat: update streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().trim().split('').reverse().join('') + '\n');
+      callback();
+    }
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import __dirname from '../path.js';
+
+const PATH = path.join(__dirname, 'streams', 'files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const file = fs.createWriteStream(PATH).on('data', () => process.stdout.write('To close the file, press Ctrl+C'));
+
+  process.stdin.on('data', data => file.write(data));
 };
 
 await write();
