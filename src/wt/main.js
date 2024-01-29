6b10b63d0dbbd85856d2757b597e7123ac1abc11
dayanych <dayana.shakenova20@gmail.com>
2024-01-29 12:24:35 +0600

    feat: update wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import os from 'os';
+import path from 'path';
+import {Worker, isMainThread} from 'node:worker_threads';
+import __dirname from '../path.js';
+
+const PATH = path.join(__dirname, 'wt', 'worker.js');
+const STARTING_NUMBER = 10;
+const NUM_CPUS = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  if (isMainThread) {
+    const result = [];
+  
+    for (let i = 0; i < NUM_CPUS; i++) {
+      const worker = new Worker(PATH, {
+        workerData: STARTING_NUMBER + i,
+      });
+
+      worker.on('message', (message) => {
+        result.push(message);
+      });
+      worker.on('error', () => {
+        result.push({
+          status: 'error',
+          data: null,
+        });
+      });
+      worker.on('exit', () => {
+        console.log(result);
+      });
+    }
+  }
 };
 
 await performCalculations();
